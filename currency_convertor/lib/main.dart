be9188b622450b4adb18404a3e96c2e77896676b
Sumit Vishwakarma <sv0042595@gmail.com>
2024-11-03 13:47:26 +0530

    New

--- currency_convertor/lib/main.dart
+++ currency_convertor/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_convertor/currency_convertor_material_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -9,14 +10,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Scaffold(
-        body: Center(
-          child: Text("Hello, World!!")
-        ),
-
-
-       )
+    return  const MaterialApp(
+      home: CurrencyConvertorMaterialPage(),
     );
   }
 }
