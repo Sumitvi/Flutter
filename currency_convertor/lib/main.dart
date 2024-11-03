599062dd74c9dac72787156a3a2a86ffa3dcaf3f
Sumit Vishwakarma <sv0042595@gmail.com>
2024-11-03 13:05:46 +0530

    New

--- currency_convertor/lib/main.dart
+++ currency_convertor/lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(Text("Hello, World!!"));
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Text("Hello Sumit!!"),
+    );
+  }
+}
+
+
+
+// There are two types of Design to be followed by the Engineerrs while Building Apps
+
+// 1. Material Design   --> Created by Google
+// 2. Coupertino Design --> Created by Apple
